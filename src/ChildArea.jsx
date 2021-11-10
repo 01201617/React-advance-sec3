1620bd5a9f767e10c78c9b25bab8398ccd42b732
Nan <40654091+01201617@users.noreply.github.com>
2021-11-10 17:37:53 +0900

    initial commit

---
+++ src/ChildArea.jsx
@@ -0,0 +1,27 @@
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open } = props;
+
+  console.log("re");
+  // 仮の重たい処理
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+});
